e76551fed813b9117d5a6cefb99ef7567282a480
Bruno Antonellini <bruno@datacoves.com>
2023-11-10 10:22:11 -0800

    Test dbt-redshift branch

--- transform/macros/tooling/generate_alias_name.sql
+++
@@ -1,9 +0,0 @@
-{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
-
-    {% if ("__" in node.name) and ("dbt_packages" not in node.root_path)%}
-        {{ node.name.split("__")[0] }}
-    {%- else -%}
-        {{ node.name }}
-    {%- endif -%} 
-
-{%- endmacro %}
