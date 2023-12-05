d8aca87512bb7a8f92e9ee48e1eb940f50ffffa7
Peter Ullrich <PJUllrich@users.noreply.github.com>
2023-12-05 15:50:02 +0100

    pip install dbt-snowflake

--- orchestrate/dags/python_sample_dag.py
+++ orchestrate/dags/python_sample_dag.py
@@ -34,7 +34,7 @@ with DAG(
     description="Sample python dag dbt run",
     schedule_interval="0 0 1 */12 *",
 ) as dag:
-    command = "source /opt/datacoves/virtualenvs/main/bin/activate && pip install git+https://github.com/datacoves/dbt-coves.git@optionally-upload-manifest-to-dbt-api"
+    command = "source /opt/datacoves/virtualenvs/main/bin/activate && pip install git+https://github.com/datacoves/dbt-coves.git@optionally-upload-manifest-to-dbt-api dbt-snowflake"
     result = subprocess.run(
         command, shell=True, check=True, executable="/bin/bash", text=True
     )
