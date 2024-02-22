1c3ee0a477fcaa3cf0a35f5753aec91902e07fb1
Noel Gomez <gomezn@convexa.ai>
2024-02-21 16:39:43 -0800

    remove env call

--- orchestrate/dags/yaml_dbt_dag.py
+++ orchestrate/dags/yaml_dbt_dag.py
@@ -18,7 +18,7 @@ from operators.datacoves.dbt import DatacovesDbtOperator
 )
 def yaml_dbt_dag():
     run_dbt = DatacovesDbtOperator(
-        task_id="run_dbt", bash_command="env | grep AIRFLOW | sort && dbt run -s personal_loans"
+        task_id="run_dbt", bash_command="dbt run -s personal_loans"
     )
 
 
