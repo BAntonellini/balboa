a0d77daebaed33948de50c4f36f02adc03fc31d4
Alejandro Morera <alejandro@datacoves.com>
2024-03-21 05:45:37 -0700

    Dags to testing dashboard

---
+++ orchestrate/dags/bash_printenv_dag.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+def run_inform_success(context):
+    print("run_inform_success")
+
+def run_inform_failure(context):
+    print("run_inform_failure")
+
+
+default_args = {
+    'owner': 'airflow',
+    "description": "Sample dag bash operator",
+    'email': 'amorera@datacoves.com',
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="bash_printenv_dag",
+    default_args=default_args,
+    start_date=datetime(2023, 8, 10),
+    catchup=False,
+    tags=["version_1"],
+    description="Sample python teams dag",
+    schedule_interval="*/10 * * * *",
+    on_success_callback=run_inform_success,
+    on_failure_callback=run_inform_failure,
+) as dag:
+
+    task_main = BashOperator(
+        task_id = "task_main",
+        bash_command = "echo \"===========| LOG_LEVEL: $LOG_LEVEL |==========\""
+    )
+
+    task_main
---
+++ orchestrate/dags/real_time_logs.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.dummy import DummyOperator
+import logging
+import time
+
+logger = logging.getLogger(__name__)
+
+default_args = {
+    'owner': 'airflow',
+    'description': "Sample python dag to check real time logs",
+    'email': 'amorera@datacoves.com',
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+with DAG(
+    dag_id = "python_sample_dag",
+    default_args = default_args,
+    start_date = datetime(2023, 8, 10),
+    catchup = False,
+    tags = ["version_5"],
+    description = "Sample python dag dbt run",
+    schedule_interval="*/10 * * * *",
+) as dag:
+
+    @task(task_id="real_time_logs")
+    def real_time_logs(**kwargs):
+        for i in range(100):
+            logger.info("Real time logs %s", i+1)
+            time.sleep(2)
+
+        return "real_time_logs finish"
+
+    start_task = DummyOperator(task_id='start_task')
+    end_task = DummyOperator(task_id='end_task')
+
+    my_real_time_logs = real_time_logs()
+    start_task >> my_task_1 >> end_task
+
+    """
+    for i in range(10):
+        @task(task_id=f"sleep_for_{i+1}")
+        def my_sleeping_function(random_base):
+            t = random_base + 15
+            return f"SLEEP {t}"
+            time.sleep(t)
+
+        sleeping_task = my_sleeping_function(random_base=i/10)
+        my_print_context >> my_task_1 >> sleeping_task
+    """
