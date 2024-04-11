296082076c9f737911704334febbc36b6a78a9f9
Bruno Antonellini <antonellinibruno@gmail.com>
2024-04-11 15:35:07 -0300

    New Notifier dag

---
+++ orchestrate/dags/new_notifier.py
@@ -0,0 +1,31 @@
+import datetime
+
+from airflow.decorators import dag, task
+from notifiers.datacoves.ms_teams.notifier import MSTeamsNotifier
+
+
+@dag(
+    default_args={
+        "start_date": datetime.datetime(2023, 1, 1, 0, 0),
+        "owner": "Bruno Antonellini",
+        "email": "bruno@datacoves.com",
+        "email_on_failure": False,
+    },
+    description="Sample DAG for dbt build",
+    schedule=None,
+    tags=["version_1"],
+    catchup=False,
+    # on_success_callback=MSTeamsNotifier(message="DAG Succeeded"),
+    # on_failure_callback=MSTeamsNotifier(message="DAG Failed"),
+)
+def new_notifier():
+    @task(
+        on_success_callback=MSTeamsNotifier(message="Success!"),
+    )
+    def t1():
+        return "hello"
+
+    t1()
+
+
+dag = new_notifier()
