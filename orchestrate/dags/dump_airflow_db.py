b3b74c0582821a8e04363dd2b7e5bb574f618d69
Noel Gomez <gomezn@convexa.ai>
2024-05-14 15:29:41 -0700

    update connection name

--- orchestrate/dags/dump_airflow_db.py
+++ orchestrate/dags/dump_airflow_db.py
@@ -11,11 +11,11 @@ from operators.datacoves.data_sync import DatacovesDataSyncOperator
 
 
 def sync_airflow_db():
-    # service connection name default is 'main_load'.
+    # service connection name default is 'airflow_db_load'.
     # Destination type default is 'snowflake' (and the only one supported for now)
     sync_data_script = DatacovesDataSyncOperator(
         destination_type="snowflake", # this can be omitted
-        service_connection_name="main_load" # this can be omitted or changed to another service connection name.
+        service_connection_name="airflow_db_load" # this can be omitted or changed to another service connection name.
     )
 
 dag = sync_airflow_db()
