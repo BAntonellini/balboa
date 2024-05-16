63e46e95942dab95fa0c44109cf9bb3fad7a8065
Bruno Antonellini <antonellinibruno@gmail.com>
2024-05-16 16:23:10 -0300

    Add snowflake customschema with tables dag

--- orchestrate/dags/snowflake_dump_airflow_tables.py
+++ orchestrate/dags/snowflake_dump_airflow_tables.py
@@ -4,15 +4,18 @@ from operators.datacoves.data_sync import DatacovesDataSyncOperatorSnowflake
 
 @dag(
     default_args={"start_date": "2021-01"},
-    description="sync_data_script",
+    description="sync_some_tables_to_custom_schema",
     schedule_interval="0 0 1 */12 *",
     tags=["version_1"],
     catchup=False,
 )
-def snowflake_sync_airflow_db():
+def snowflake_sync_airflow_tables():
     # service connection name default is 'airflow_db_load'.
     # Destination type default is 'snowflake' (and the only one supported for now)
-    sync_entire_db = DatacovesDataSyncOperatorSnowflake()
+    sync_some_tables_to_custom_schema = DatacovesDataSyncOperatorSnowflake(
+        destination_schema="BRUNO_TABLES_DUMP",
+        tables=["task_fail", "task_instance"],
+    )
 
 
-dag = snowflake_sync_airflow_db()
+dag = snowflake_sync_airflow_tables()
