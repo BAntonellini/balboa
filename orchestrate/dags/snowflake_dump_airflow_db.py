9841dc5f545220565051007c9c9fc85846a418c9
Bruno Antonellini <antonellinibruno@gmail.com>
2024-05-29 17:49:08 -0300

    Change SC name

--- orchestrate/dags/snowflake_dump_airflow_db.py
+++ orchestrate/dags/snowflake_dump_airflow_db.py
@@ -6,11 +6,11 @@ from operators.datacoves.data_sync import DatacovesDataSyncOperatorSnowflake
     default_args={"start_date": "2021-01"},
     description="sync_data_script",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_1"],
+    tags=["version_2"],
     catchup=False,
 )
 def snowflake_sync_airflow_db():
-    sync_entire_db = DatacovesDataSyncOperatorSnowflake()
+    sync_entire_db = DatacovesDataSyncOperatorSnowflake(service_connection_name="main")
 
 
 dag = snowflake_sync_airflow_db()
