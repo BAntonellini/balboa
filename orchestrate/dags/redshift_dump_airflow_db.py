fa03cdb4f9c4c2308d55276154bcf4e9c87e6796
Bruno Antonellini <antonellinibruno@gmail.com>
2024-05-30 12:29:59 -0300

    Try complete dump

--- orchestrate/dags/redshift_dump_airflow_db.py
+++ orchestrate/dags/redshift_dump_airflow_db.py
@@ -6,12 +6,14 @@ from operators.datacoves.data_sync import DatacovesDataSyncOperatorRedshift
     default_args={"start_date": "2021-01"},
     description="sync_entire_db",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_3"],
+    tags=["version_4"],
     catchup=False,
 )
 def redshift_sync_airflow_db():
     sync_entire_db = DatacovesDataSyncOperatorRedshift(
-        service_connection_name="redshift", additional_tables=["log", "log_template"]
+        service_connection_name="redshift",
+        tables=[],
+        destination_schema="is-this-complete",
     )
 
 
