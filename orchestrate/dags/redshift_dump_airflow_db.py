c41c3e846e4e868bff11c502774d7d6e24b65a37
Bruno Antonellini <antonellinibruno@gmail.com>
2024-05-30 11:00:11 -0300

    Bump dag version

--- orchestrate/dags/redshift_dump_airflow_db.py
+++ orchestrate/dags/redshift_dump_airflow_db.py
@@ -6,7 +6,7 @@ from operators.datacoves.data_sync import DatacovesDataSyncOperatorRedshift
     default_args={"start_date": "2021-01"},
     description="sync_entire_db",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_1"],
+    tags=["version_2"],
     catchup=False,
 )
 def redshift_sync_airflow_db():
--- orchestrate/dags/redshift_dump_airflow_tables.py
+++ orchestrate/dags/redshift_dump_airflow_tables.py
@@ -6,7 +6,7 @@ from operators.datacoves.data_sync import DatacovesDataSyncOperatorRedshift
     default_args={"start_date": "2021-01"},
     description="sync_some_tables",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_1"],
+    tags=["version_2"],
     catchup=False,
 )
 def redshift_sync_airflow_tables():
