f64b44d8908bb0ab7f5b9760ceb40439344d815d
Bruno Antonellini <antonellinibruno@gmail.com>
2024-05-30 11:13:08 -0300

    Version 3 of dump

--- orchestrate/dags/redshift_dump_airflow_db.py
+++ orchestrate/dags/redshift_dump_airflow_db.py
@@ -6,12 +6,12 @@ from operators.datacoves.data_sync import DatacovesDataSyncOperatorRedshift
     default_args={"start_date": "2021-01"},
     description="sync_entire_db",
     schedule_interval="0 0 1 */12 *",
-    tags=["version_2"],
+    tags=["version_3"],
     catchup=False,
 )
 def redshift_sync_airflow_db():
     sync_entire_db = DatacovesDataSyncOperatorRedshift(
-        service_connection_name="redshift"
+        service_connection_name="redshift", additional_tables=["log", "log_template"]
     )
 
 
