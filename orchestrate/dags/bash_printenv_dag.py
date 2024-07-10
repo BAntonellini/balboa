64940897339fbfc70b96afd25e0d76a48eb60c1e
Datacoves Admin <hey@datacoves.com>
2024-07-10 10:35:10 -0700

    Eat memory

--- orchestrate/dags/bash_printenv_dag.py
+++ orchestrate/dags/bash_printenv_dag.py
@@ -28,7 +28,7 @@ with DAG(
     default_args=default_args,
     start_date = datetime(2024, 7, 10),
     catchup=False,
-    tags=["version_6"],
+    tags=["version_1"],
     description="Sample dag",
     schedule_interval="*/10 * * * *",
     on_success_callback=run_inform_success,
