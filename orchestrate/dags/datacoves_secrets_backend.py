2be1d4a2f9b36e75bee50051c7fc3bb4513bb15c
Bruno Antonellini <antonellinibruno@gmail.com>
2024-07-18 11:25:06 -0300

    Datacoves Secrets Backend dag

---
+++ orchestrate/dags/datacoves_secrets_backend.py
@@ -0,0 +1,46 @@
+from airflow.decorators import dag
+from airflow.models import Variable
+from operators.datacoves.bash import DatacovesBashOperator
+from pendulum import datetime
+
+snowflake_password = Variable.get("snowflake-password")
+all_passwords = Variable.get("all-passwords")
+
+
+@dag(
+    default_args={
+        "start_date": datetime(2022, 10, 10),
+        "owner": "Bruno Antonellini",
+        "email": "gomezn@example.com",
+        "email_on_failure": True,
+    },
+    catchup=False,
+    tags=["version_1"],
+    description="Datacoves Secrets Backend dag",
+    # This is a regular CRON schedule. Helpful resources
+    # https://cron-ai.vercel.app/
+    # https://crontab.guru/
+    schedule_interval="0 0 1 */12 *",
+)
+def datacoves_secrets_backend():
+    # Calling dbt commands
+    echo_simple_secret = DatacovesBashOperator(
+        task_id="echo_simple_secret",
+        bash_command=f"echo {snowflake_password}",
+    )
+
+    # This is calling an external Python file after activating the venv
+    # use this instead of the Python Operator
+    echo_complex_secret = DatacovesBashOperator(
+        task_id="echo_complex_secret",
+        # Virtual Environment is automatically activated
+        # activate_venv=True,
+        bash_command=f"echo {all_passwords}",
+    )
+
+    # Define task dependencies
+    echo_complex_secret.set_upstream([echo_simple_secret])
+
+
+# Invoke Dag
+dag = datacoves_secrets_backend()
