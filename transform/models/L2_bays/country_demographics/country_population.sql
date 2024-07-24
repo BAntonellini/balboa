ccf358a3f860c60ba9df8ad3f44ccc5effab5581
Bruno Antonellini <antonellinibruno@gmail.com>
2024-07-24 09:28:24 -0300

    Test checkpoint action

--- transform/models/L2_bays/country_demographics/country_population.sql
+++ transform/models/L2_bays/country_demographics/country_population.sql
@@ -1,18 +1,16 @@
 with population_rank as (
     select
         country_code,
-        country_name,
         value,
         year,
         rank() over (
-            partition by country_code, country_name order by year desc
+            partition by country_code, order by year desc
         ) as rank_years
     from {{ ref('country_populations') }}
 )
 
 select
     country_code,
-    country_name,
     value as total_population,
     year
 from population_rank
